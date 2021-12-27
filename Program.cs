7abe0ea16e8bf3d6e963c8b54fbcac880d2d4806
Grymnir <magnus.carl.ekman@gmail.com>
2021-12-27 01:28:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PalindromeCheckerUserinput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Palindromchecker();
+            while (true)
+            {
+                Palindromchecker();
+            }
+        }
+        private static void Palindromchecker()
+        {
+            int rem, sum = 0, saved;
+            Console.WriteLine("Please add a number: ");
+            int palin = Convert.ToInt32(Console.ReadLine());
+            saved = palin;
+            while (palin > 0)
+            {
+                rem = palin % 10;
+                palin = palin / 10;
+                sum = sum * 10 + rem;
+            }
+            if (saved == sum)
+            {
+                Console.WriteLine("Its a palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("Its not a palindrom!");
+            }
+        }
+    }
+}
